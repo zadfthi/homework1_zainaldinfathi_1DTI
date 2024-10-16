77701c9612dfcfa85b464494955fe03e3a0740b7
zadfthi <zainaldinfathi@gmail.com>
2024-10-16 12:02:45 +0700

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello Word')
+# Program Alat Bantu Pembuangan Sampah yang Benar!
+# Program ini dituju untuk Masyarakat yang masih membuang sampah sembarangan
+
+print('Silahkan Pilih Tipe Sampah yang Ingin Anda Ketahui!')
+print('1. Organik', '2. Anorganik', '3. B3')
+
+tipeSampah = {'Organik', 'Anorganik', 'B3'}
+dataSampah = int(input ('Silahkan Masukkan Nomor Tentang Sampah yang Ingin Anda Ketahui! : '))
+
+if 'Organik' in tipeSampah: (
+    list (int = [1, 2, 3, 4])
+    for i in int
+)
+
+
+dict 
